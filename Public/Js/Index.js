7d47f7f647a2333f89369b7c800cab4f002dfc71
Ademola <adeakanfea@gmail.com>
2020-12-14 23:57:32 +0100

    =Trying to fix error3

--- Public/Js/Index.js
+++ Public/Js/Index.js
@@ -1,26 +1,23 @@
 let Form=document.querySelector('form');
-Form.addEventListener('click',()=>{
-    console.log('Ademola');
+let value=document.querySelector('input');
+let output=document.querySelector('.values1');
+let template='';
+Form.addEventListener('submit',e=>{
+    e.preventDefault();
+    output.innerHTML="Loading";
+template='loading';
+fetch("/weather?search="+value.value)
+.then(response=>{
+    response.json().then(data=>{
+        if(data.error){
+            template=data.error;
+            output.innerHTML=template;
+        }
+        else{
+            template="The current Temperature is "+data.temp_c+",it is "+data.condition.text;
+        output.innerHTML=template;
+        }
+    });
 });
-// let value=document.querySelector('input');
-// let output=document.querySelector('.values1');
-// let template='';
-// Form.addEventListener('submit',e=>{
-//     e.preventDefault();
-//     output.innerHTML="Loading";
-// template='loading';
-// fetch("/weather?search="+value.value)
-// .then(response=>{
-//     response.json().then(data=>{
-//         if(data.error){
-//             template=data.error;
-//             output.innerHTML=template;
-//         }
-//         else{
-//             template="The current Temperature is "+data.temp_c+",it is "+data.condition.text;
-//         output.innerHTML=template;
-//         }
-//     });
-// });
 
-// });
+});
