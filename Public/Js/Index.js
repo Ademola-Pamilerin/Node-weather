18b5679612249634cc8b95051cb8372750448728
Ademola <adeakanfea@gmail.com>
2020-12-14 23:49:45 +0100

    =Trying to fix error2

--- Public/Js/Index.js
+++ Public/Js/Index.js
@@ -1,24 +1,26 @@
-let Weather=document.querySelector('form');
-let value=document.querySelector('input');
-let output=document.querySelector('.values1');
-Weather.addEventListener('submit',e=>{
-    e.preventDefault();
-    output.innerHTML="Loading";
-let template='loading';
-fetch("/weather?search="+value.value)
-.then(response=>{
-    response.json().then(data=>{
-        if(data.error){
-           // console.log(data.error);
-            template=data.error;
-            output.innerHTML=template;
-        }
-        else{
-            //console.log(data);
-            template="The current Temperature is "+data.temp_c+",it is "+data.condition.text;
-        output.innerHTML=template;
-        }
-    });
+let Form=document.querySelector('form');
+Form.addEventListener('click',()=>{
+    console.log('Ademola');
 });
+// let value=document.querySelector('input');
+// let output=document.querySelector('.values1');
+// let template='';
+// Form.addEventListener('submit',e=>{
+//     e.preventDefault();
+//     output.innerHTML="Loading";
+// template='loading';
+// fetch("/weather?search="+value.value)
+// .then(response=>{
+//     response.json().then(data=>{
+//         if(data.error){
+//             template=data.error;
+//             output.innerHTML=template;
+//         }
+//         else{
+//             template="The current Temperature is "+data.temp_c+",it is "+data.condition.text;
+//         output.innerHTML=template;
+//         }
+//     });
+// });
 
-});
+// });
